8babb5381802dca3e80986ef4e05727a5bc91cea
지유민 <yuminji97@kakao.com>
2024-07-22 13:00:24 +0900

    update

--- lib/screens/mypage/main.dart
+++ lib/screens/mypage/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFf0f0f0),
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Color(0xFF10D9B5),
         elevation: 0,
-        toolbarHeight: 140,
+        toolbarHeight: 250,
         flexibleSpace: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
@@ -18,79 +18,324 @@ class MyPage extends StatelessWidget {
               end: Alignment.bottomCenter,
             ),
           ),
-          child: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 40,
-                  backgroundImage: NetworkImage(
-                      'https://via.placeholder.com/150'), // Replace with your image URL
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '홍길동',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+          child: Column(
+            children: [
+              Container(
+                margin:
+                    EdgeInsets.only(top: 25, right: 10, left: 10, bottom: 5),
+                child: Center(
+                  child: Text(
+                    'MY',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-                SizedBox(height: 5),
-                Text(
-                  'asdf@naver.com',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white70,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 60,
+                      height: 60,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/avatar_man.png'),
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '회사 아이콘',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            '이름',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            '이메일',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          '계정',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(''),
+                        Text(''),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF028490),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '폴리시',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          '지정 안함',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Icon(
+                      Icons.chevron_right,
+                      color: Colors.white,
+                      size: 24,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 8),
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Container(
+                  color: Color(0xFFf0f0f0),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      FeatureSettingSection2(),
+                      FeatureSettingSection3(),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-        ),
+        ],
       ),
-      body: ListView(
+    );
+  }
+}
+
+class FeatureSettingSection2 extends StatefulWidget {
+  @override
+  _FeatureSettingSection2State createState() => _FeatureSettingSection2State();
+}
+
+class _FeatureSettingSection2State extends State<FeatureSettingSection2> {
+  bool _isWiFiUploadOnly = false;
+  bool _isImageAutoSave = false;
+  bool _isStartWithCameraMode = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.white,
+      padding: EdgeInsets.only(left: 24, top: 8, right: 15, bottom: 8),
+      margin: EdgeInsets.only(bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Profile'),
-            onTap: () {
-              // Profile page navigation
-            },
+          Text(
+            '기능설정',
+            style: TextStyle(fontSize: 12, color: Color(0xFF333333)),
           ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
-            onTap: () {
-              // Settings page navigation
-            },
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'wi-fi upload only',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              CustomSwitch(
+                value: _isWiFiUploadOnly,
+                onChanged: (bool newValue) {
+                  setState(() {
+                    _isWiFiUploadOnly = newValue;
+                  });
+                },
+              ),
+            ],
           ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.notifications),
-            title: Text('Notifications'),
-            onTap: () {
-              // Notifications page navigation
-            },
+          Divider(color: Color.fromARGB(255, 241, 241, 241)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '이미지 자동 저장',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              CustomSwitch(
+                value: _isImageAutoSave,
+                onChanged: (bool newValue) {
+                  setState(() {
+                    _isImageAutoSave = newValue;
+                  });
+                },
+              ),
+            ],
           ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.help),
-            title: Text('Help & Support'),
-            onTap: () {
-              // Help & Support page navigation
-            },
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.logout),
-            title: Text('Logout'),
-            onTap: () {
-              // Logout action
-            },
+          Divider(color: Color.fromARGB(255, 241, 241, 241)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '카메라 모드로 시작',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              CustomSwitch(
+                value: _isStartWithCameraMode,
+                onChanged: (bool newValue) {
+                  setState(() {
+                    _isStartWithCameraMode = newValue;
+                  });
+                },
+              ),
+            ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class FeatureSettingSection3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.white,
+      padding: EdgeInsets.only(left: 24, top: 8, right: 30, bottom: 8),
+      margin: EdgeInsets.only(bottom: 10),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '앱 정보',
+            style: TextStyle(fontSize: 12, color: Color(0xFF333333)),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '버전',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '1.0.0',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+          Divider(color: Color.fromARGB(255, 241, 241, 241)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Help & Feedback',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Icon(
+                Icons.chevron_right,
+                size: 24,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomSwitch extends StatelessWidget {
+  final bool value;
+  final ValueChanged<bool> onChanged;
+  final double scale;
+
+  const CustomSwitch({
+    required this.value,
+    required this.onChanged,
+    this.scale = 0.8, // Default scale is 1.0
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Transform.scale(
+        scale: scale,
+        child: CupertinoSwitch(
+          value: value,
+          onChanged: onChanged,
+          activeColor: Color(0xFF10D9B5),
+          trackColor: Color(0xFFE1E1E1),
+        ),
       ),
     );
   }
