49fcb9a2a18d75784c6e3563f7bd3708726bdc33
지유민 <yuminji97@kakao.com>
2024-08-09 17:13:21 +0900

    update

--- lib/screens/receipt/main.dart
+++ lib/screens/receipt/main.dart
@@ -174,7 +174,7 @@ class ReceiptsPage extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.only(bottom: 70),
         child: ListView.builder(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
           itemCount: dummyData.length,
           itemBuilder: (context, index) {
             return CustomCard(
